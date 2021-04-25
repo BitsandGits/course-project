f3140d4110c36ebc32dc83f159ab05601d0b81d7
BitsandGits <joyjingdesign@gmail.com>
2021-04-24 22:18:27 -0400

    hw8 added javascript file and hamburger menu icon actions

---
+++ public/scripts/scripts.js
@@ -0,0 +1,9 @@
+// JAVASCRIPT FOR COURSE PROJECT 
+
+// hamburger menu icon show/hide items 
+let hamburgerMenuIcon = document.querySelector(".menu");
+let menuItems = document.querySelector(".topnav");
+let toggleMenu = function() {
+    menuItems.classList.toggle("open");
+}
+hamburgerMenuIcon.addEventListener("click",toggleMenu);
