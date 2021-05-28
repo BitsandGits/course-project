8d9a60d67e76f039ae0e4b4f966b8cd26d7ae19c
BitsandGits <joyjingdesign@gmail.com>
2021-05-27 23:28:47 -0400

    hw15 connect to MongoDB Atlas and .env

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/codesquadComics", 
-{useNewUrlParser:true, useUnifiedTopology:true},
-(err) => {
-    if(!err) { 
-        console.log("Successful connection with MongoDB Server. Good job!");
-    } else {
-        console.log("Error with MongoDB connectivity");
-    }
+mongoose.connect(
+    process.env.DB_URL, // use .env
+    {useNewUrlParser:true, useUnifiedTopology:true},
+    (err) => {
+        if(!err) { 
+            console.log("Successful connection with MongoDB Server. Good job!");
+        } else {
+            console.log("Error with MongoDB connectivity");
+        }
 });
